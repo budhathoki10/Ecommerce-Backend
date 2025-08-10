d9634efa5e28e18888cae4bde7841e0ed9f34c87
budhathoki10 <budhathokikushal170@gmail.com>
2025-08-10 16:48:24 +0545

    added a intermediate cart like showing total price auantity etc

--- controller/Customer/Order.js
+++ controller/Customer/Order.js
@@ -1,32 +1,37 @@
 const cart= require("../../models/Cart.models.js");
 const items= require("../../models/items.models.js");
 const userdetail = require("../../models/userdetails.js");
+let totalPrice;
 
 
 const addToCart = async (req, res) => {
     
     try {
-        const { productId,userdetails} = req.body;
+        const {userdetails,Quantity,status,productName} = req.body;
         const userId = req.user?.id;
         // check if tem prensent in database
-        const checkitems= await items.findOne({productId:productId})
-        if (!checkitems) {
+       const id= req?.params?.id
+       const checkItems= await items.findById(id)
+        if (!checkItems) {
             return res.status(400).json({ message: "cannot find this item in datbase" });
         }
-        console.log("hello");
+   
         // check cart 
-            const checkcart= await cart.findOne({productId:productId})
-        if (checkcart) {
-            return res.status(400).json({ message: "already present in cart" });
+        if(Quantity<=0){
+            return res.status(400).json({message:"please use 1 or more Quantitiy"})
         }
-        
+        let totalPrice= Number(checkItems.Price)*Number(Quantity)
+        let name= checkItems.productName
         const newCartItem = new cart({
-            productId,
+            productName:name,
+            Quantity,
+            totalPrice,
+            status,
             userdetails:userId
         });
-
+                                    
         await newCartItem.save();
-        res.status(201).json({ message: "Item added to cart successfully", item: newCartItem });
+        res.status(201).json({ message: `Item added to cart successfully: ${checkItems.productName} is added in database`});
     } catch (error) {
         console.error("Error adding item to cart:", error);
         res.status(500).json({ message: "Internal Server Error dsd" });
@@ -34,8 +39,8 @@ const addToCart = async (req, res) => {
 }
 const viewTocart= async (req,res) => {
     try {
+       
         const viewcart = await cart.find().populate("userdetails", "firstName lastName phonenumber email ")
-        .populate("productdetails", "productName");
         if(viewcart.length === 0){
         return res.status(400).json({message:"no any order"})
     }
--- models/Cart.models.js
+++ models/Cart.models.js
@@ -2,20 +2,35 @@ const mongoose= require("mongoose")
 const productdetails = require("./items.models")
 const cartSystem= mongoose.Schema({
     
-    productId:{
+    // productId:{
+    //     type:Number,
+    //     required:true
+    // },
+    productName:{
+        type:String,
+    },
+    Quantity:{
         type:Number,
         required:true
     },
+    totalPrice:{
+         type:Number,
+    },
+    status:{
+        type:String,
+        default:"pending"
+    },
+
     userdetails:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "userDetails",
         required:true
-},
-    productdetails:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "productdetails",
-        required:true
 }
+//     productdetails:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "productdetails",
+//         required:true
+// }
     })
 const cart= mongoose.model("cartSystem",cartSystem)
 module.exports= cart
