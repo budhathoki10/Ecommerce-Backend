82282f8927897b4c8dfa70cbe919b8581fc8416b
budhathoki10 <budhathokikushal170@gmail.com>
2025-08-22 20:13:55 +0545

    setup prettier

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const router= require("./middleware/ErrorHanlder.middleware")
 const cookie= require("cookie-parser")
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
+const cors= require("cors")
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 mongoose.connect(process.env.MONGOOSE_URL).then(()=>{
